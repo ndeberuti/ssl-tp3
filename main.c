cf4bbeba3d8a377228d6b1f9ce4dfacdcd88adc6
Nicolas de Beruti <saxo_nico@hotmail.com>
2017-09-15 19:22:50 -0300

    Sacar acentos de caratula

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
-//Profesor Eduardo Zúñiga
+//Profesor Eduardo Zuñiga
 //Curso K2055
 //Grupo N°1
 //TP N°3
 //Integrantes: Luna, Brian Damian 155.369-0
 //Martin, Rodrigo Leonardo 160.255-0
 //Miravalles, Emanuel Gonzalo 127.099-0
-//de Beruti, Nicolás Alejandro 149.700-5
+//de Beruti, Nicolas Alejandro 149.700-5
